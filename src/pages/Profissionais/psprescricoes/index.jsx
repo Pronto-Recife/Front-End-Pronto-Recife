76b9e6227c9fa98dfb6df503a2ced8d970375f80
myllercardoso <myller.mil@gmail.com>
2024-11-27 22:09:16 -0300

    feet: prescricoes medicas created

--- src/pages/Profissionais/psprescricoes/index.jsx
+++ src/pages/Profissionais/psprescricoes/index.jsx
@@ -7,39 +7,44 @@ import * as S from "./styles";
 
 export default function Psprescricoes() {
     return (
-        <>  
-        <S.Container>
+        <>
             <Superiorbar />
-            <Sidebarprofissionais />
 
-            
-                <S.Header>
-                    <h1>Prescrição médica</h1>
-                    <S.SearchWrapper>
-                        <S.SearchInput type="text" placeholder="Id do paciente, Nº do Prontuário" />
-                        <Search size={24} color="#161B68" />
-                    </S.SearchWrapper>
-                </S.Header>
+            <S.Container>
+                <S.Sidebar>
+                    <Sidebarprofissionais />
+                </S.Sidebar>
 
-                <S.ProfileSection>
-                    <img src={Foto} alt="Foto de perfil" />
-                    <S.Info>
-                        <h2>Maria Lucia da Silva</h2>
-                        <p><strong>Nº Prescrição:</strong> 1115</p>
-                        <p><strong>Paciente:</strong> Maria Lucia Da Silva</p>
-                        <p><strong>Idade:</strong> 68</p>
-                        <p><strong>Sexo:</strong> Feminino</p>
-                        <p><strong>Motivo do internamento:</strong> BCP</p>
-                        <p><strong>Diagnóstico:</strong> AVC PRÉVIO + HEMIPLEGIA D</p>
-                        <p><strong>Alergias:</strong> Nada consta</p>
-                        <p><strong>Convênio:</strong> Público</p>
-                        <p><strong>Médico:</strong> Dr. Roberto Lima</p>
-                    </S.Info>
-                </S.ProfileSection>
+                <S.MainContent>
+                    <S.Header>
+                        <h1>Prescrição médica</h1>
+                        <S.SearchWrapper>
+                            <S.SearchInput type="text" placeholder="Id do paciente, Nº do Prontuário" />
+                            <Search size={24} color="#161B68" />
+                        </S.SearchWrapper>
+                    </S.Header>
 
-                <S.Prescriptions>
-                    {[1, 2].map((_, index) => (
-                        <S.PrescriptionCard key={index}>
+                    <S.ProfileSection>
+                        <img src={Foto} alt="Foto de perfil" />
+                        <S.Info>
+                            <div>
+                                <p><strong>Nº Prescrição:</strong> 1115</p>
+                                <p><strong>Paciente:</strong> Maria Lucia Da Silva</p>
+                                <p><strong>Idade:</strong> 68</p>
+                                <p><strong>Sexo:</strong> Feminino</p>
+                            </div>
+                            <div>
+                                <p><strong>Motivo do internamento:</strong> BCP</p>
+                                <p><strong>Diagnóstico:</strong> AVC PRÉVIO + HEMIPLEGIA D</p>
+                                <p><strong>Alergias:</strong> Nada consta</p>
+                                <p><strong>Convênio:</strong> Público</p>
+                                <p><strong>Médico:</strong> Dr. Roberto Lima</p>
+                            </div>
+                        </S.Info>
+                    </S.ProfileSection>
+
+                    <S.Prescriptions>
+                        <S.PrescriptionCard>
                             <h3>Medicamentos</h3>
                             <ul>
                                 <li>AAS 100 mg Comprido Similar, <b>Administrar 1 cp</b></li>
@@ -50,21 +55,42 @@ export default function Psprescricoes() {
                                 <li>8:00 - 16h</li>
                                 <li>8:00 - 16h</li>
                             </ul>
-                            <h4>Observações</h4>
+                            <h3>Observações</h3>
                             <ul>
                                 <li>Esquema de soro: correr durante 8 horas.</li>
                                 <li>Repouso relativo no leito.</li>
                             </ul>
                         </S.PrescriptionCard>
-                    ))}
-                </S.Prescriptions>
 
-                <S.VejaMaisButton>
-                    <CirclePlus size={24} color="#161B68" />
-                    Veja mais
-                </S.VejaMaisButton>
+                        <S.PrescriptionCard>
+                            <h3>Medicamentos</h3>
+                            <ul>
+                                <li>AAS 100 mg Comprido Similar, <b>Administrar 1 cp</b></li>
+                                <li>Digoxina 0,5 mg Comprimido Marca, <b>Administrar 1 cp</b></li>
+                            </ul>
+                            <h3>Horários</h3>
+                            <ul>
+                                <li>8:00 - 16h</li>
+                                <li>8:00 - 16h</li>
+                            </ul>
+                            <h3>Observações</h3>
+                            <ul>
+                                <li>Esquema de soro: correr durante 8 horas.</li>
+                                <li>Repouso relativo no leito.</li>
+                            </ul>
+                        </S.PrescriptionCard>
+
+                        <S.VejaMaisButton>
+                            <CirclePlus size={24} color="#161B68" />
+                            Veja mais
+                        </S.VejaMaisButton>
+                    </S.Prescriptions>
+                </S.MainContent>
             </S.Container>
 
+            <footer>
+                <Footerbar />
+            </footer>
         </>
     );
 }
--- src/pages/Profissionais/psprescricoes/styles.js
+++ src/pages/Profissionais/psprescricoes/styles.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: cen;
+  display: grid;
   grid-template-rows: auto 1fr auto;
   grid-template-columns: 250px 1fr;
   grid-template-areas:
@@ -21,31 +19,44 @@ export const Container = styled.div`
   }
 `;
 
-export const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 2rem;
+export const Sidebar = styled.div`
+  grid-area: sidebar;
 
+`;
+
+export const MainContent = styled.main`
+  grid-area: main;
+  padding: 20px;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 80px 20px 20px 0px;
+  margin-left: 50px;
+  
   h1 {
-    margin-bottom: 1rem;
+    font-size: 35px;
+    font-weight: bold;
     color: #161B68;
+    white-space: nowrap;
   }
 `;
 
 export const SearchWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
   padding: 1rem;
-  background-color: #f0f0f0;
-  border-radius: 8px;
+  background-color: #f1f1f1;
+  border-radius: 30px;
+  margin-right: 50px;
 `;
 
 export const SearchInput = styled.input`
   width: 100%;
   padding: 0.5rem;
-  border: 1px solid #ccc;
+  border: 1px solid #c1c1c1;
   border-radius: 20px;
   font-size: 1rem;
   margin-right: 0.5rem;
@@ -54,33 +65,39 @@ export const SearchInput = styled.input`
 export const ProfileSection = styled.div`
   display: flex;
   align-items: center;
-  margin-bottom: 2rem;
+  margin-top: 30px;
+  margin-bottom: 30px;
 
   img {
+    margin-left: 50px;
     width: 150px;
     height: 150px;
     border-radius: 50%;
     margin-right: 2rem;
+
   }
 `;
 
 export const Info = styled.div`
-  h2 {
-    margin-bottom: 1rem;
-    color: #161B68;
+  display: flex;
+  flex-wrap: wrap;
+
+  div {
+    
+    flex: 1;
+    margin-right: 1rem;
   }
 
   p {
     margin: 0.3rem 0;
-  }
-
-  strong {
     color: #161B68;
   }
 `;
 
 export const Prescriptions = styled.div`
   margin-bottom: 2rem;
+  margin-left: 50px;
+  margin-right: 50px;
 `;
 
 export const PrescriptionCard = styled.div`
@@ -91,12 +108,8 @@ export const PrescriptionCard = styled.div`
 
   h3 {
     color: #161B68;
-    margin-bottom: 0.5rem;
-  }
-
-  h4 {
-    margin-top: 1rem;
-    color: #161B68;
+    display: inline-block;
+    margin-right: 1rem;
   }
 
   ul {
@@ -110,18 +123,19 @@ export const PrescriptionCard = styled.div`
 
 export const VejaMaisButton = styled.button`
   display: flex;
-  flex-direction: column;
+  justify-content: flex-end;
   align-items: center;
-  justify-content: center;
   padding: 0.5rem 1rem;
   background-color: transparent;
   color: #030F41; 
+  font-weight: 600;
   border: none;
   border-radius: 4px;
   font-size: 1rem;
   cursor: pointer;
   margin-top: 1rem;
   transition: transform 0.3s ease, background-color 0.3s ease;
+  align-self: flex-end;
 
   &:hover {
     background-color: none; 
@@ -129,6 +143,8 @@ export const VejaMaisButton = styled.button`
   }
 
   svg {
-    margin-bottom: 0.5rem;
+    margin-right: 0.5rem;
   }
 `;
+
+
